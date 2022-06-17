61d75a23f5cb57cb442a53220ef82b571c93cb0e
Dallas Hepner <dallashepner@gmail.com>
2022-06-17 14:16:18 -0600

    Problem 2 of variables

--- variables.js
+++ variables.js
@@ -6,7 +6,7 @@ let myName = `Dallas Hepner`;
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
-//CODE HERE
+let faveNum = 3;
 
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
